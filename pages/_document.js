2c5a526014136131ed955e1b66fefbc91de34d5a
Ncp-Zg <78085932+Ncp-Zg@users.noreply.github.com>
2022-10-01 19:40:17 +0300

    test_1

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            async
+            defer
+            data-website-id="2cf7e2a2-1316-457e-b461-35118f3a795c"
+            src="https://umami-2.herokuapp.com/umami.js"
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
