9ef1b1e42499bed3e033678556fab4564e4ed37f
unknown <ncptzgn@gmail.com>
2022-11-22 14:04:44 +0300

    track visit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,28 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { useAckee } from 'use-ackee'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      useAckee(url, {
+        server: 'https://your.app',
+        domainId: 'your-domain-id'
+      }, {
+        detailed: false,
+        ignoreLocalhost: true,
+        ignoreOwnVisits: true
+      })      
+    }
+      router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+        router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return <Component {...pageProps} />
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <script async src="https://track-visit.vercel.app/tracker.js" data-ackee-server="https://serene-sfogliatella-92f81b.netlify.app" data-ackee-domain-id="318f89b4-d620-4882-871e-ca048e3e8d28"></script>
+          <script async src="https://serene-sfogliatella-92f81b.netlify.app/tracker.js" data-ackee-server="https://serene-sfogliatella-92f81b.netlify.app" data-ackee-domain-id="318f89b4-d620-4882-871e-ca048e3e8d28"></script>
         </Head>
         <body>
           <Main />
