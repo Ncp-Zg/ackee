65ffd3f7830cacbbd4cdf1e8d388ec3ede945940
unknown <ncptzgn@gmail.com>
2022-11-22 13:40:53 +0300

    ackee

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <script
-            async
-            defer
-            data-website-id="2cf7e2a2-1316-457e-b461-35118f3a795c"
-            src="https://umami-2.herokuapp.com/umami.js"
-          ></script>
+        <script async src="https://serene-sfogliatella-92f81b.netlify.app/tracker.js" data-ackee-server="https://serene-sfogliatella-92f81b.netlify.app" data-ackee-domain-id="318f89b4-d620-4882-871e-ca048e3e8d28"></script>
         </Head>
         <body>
           <Main />
